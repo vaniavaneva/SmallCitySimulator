040573629f1d8666fe19fbc3305d6e433e3fa14b
Vania <veiniatodorova@abv.bg>
2025-12-05 14:57:12 +0200

    Revert "no message"
    This reverts commit 9a91054c93bdf307fc9c3b091576ad84705ff950.

--- src/city/CityController.java
+++ src/city/CityController.java
@@ -6,7 +6,15 @@ import devices.CityDevice;
 public class CityController {
     private static CityController instance;
 
+    private CityThreadPool pool;
 
+    public void setThreadPool(CityThreadPool pool){
+        this.pool = pool;
+    }
+
+    public CityThreadPool getThreadPool() {
+        return pool;
+    }
 
     private CityController() {}
     public static CityController getInstance() {
@@ -14,5 +22,11 @@ public class CityController {
             instance = new CityController();
         }
         return instance;
+    }
+
+    public void startSimulation(City city) {
+        for(CityDevice device : devices){
+            device.schedule(pool);
+        }
     }
 }
